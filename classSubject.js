c3c1b66722131ec17f09086911ffa5a6db6440ec
unknown <ganijustine8@gmail.com>
2024-11-04 17:44:15 +0800

    initial commit

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "DSA - Data Structures and Algorithms";
+let classSchedule = "MF 10:00 AM - 11:00 AM";
+let classroom = "Cor A";
+let classInstructor = "Dr. Vj Barayuga";
+let studentName = "Justine Gani ";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
